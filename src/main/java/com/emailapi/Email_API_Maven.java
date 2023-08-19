a59bac45297f66581abe4d80995d085c091af996
krtuhin <tuhin.km2774@gmail.com>
2023-08-19 19:32:25 +0530

    Send Email with Attachment

--- src/main/java/com/emailapi/Email_API_Maven.java
+++ src/main/java/com/emailapi/Email_API_Maven.java
@@ -8,13 +8,16 @@ public class Email_API_Maven {
 
         System.out.println("Sending Email...!");
 
-        String message = "Hello! This message for security check...!";
+        String message = "Hello! This message for sending attachment..!";
         String subject = "Verification Mail";
         String to = "receiver email address";
         String from = "sender email address";
 
         //sending only text
         SendEmail.sendTxst(message, subject, to, from);
+
+        //sending attachment with text
+        SendEmail.sendAttachement(message, subject, from, to);
 
         System.out.println("Email sent successfully...!!");
 
--- src/main/java/com/emailapi/email/SendEmail.java
+++ src/main/java/com/emailapi/email/SendEmail.java
@@ -1,5 +1,6 @@
 package com.emailapi.email;
 
+import java.io.File;
 import java.util.Properties;
 import javax.mail.Authenticator;
 import javax.mail.Message;
@@ -7,11 +8,13 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class SendEmail {
 
-    //authentication username and password
+    //username & password for sender authentication
     private static String userName = "sender email address without '@gmail.com'";
     private static String password = "app password created in gmail account";
 
@@ -70,4 +73,78 @@ public class SendEmail {
         }
     }
 
+    //method for send email with attachment
+    public static void sendAttachement(String message, String subject, String from, String to) {
+
+        //host server of gmail
+        String host = "smtp.gmail.com";
+
+        //properties object
+        Properties properties = System.getProperties();
+
+        //set email server properties
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", 587);
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+
+        //get session for sending email
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        });
+
+        //compose message
+        Message m = new MimeMessage(session);
+
+        try {
+            //set message properties
+
+            //setting sender into message object
+            m.setFrom(new InternetAddress(from));
+
+            //setting receiver into message object
+            m.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            //setting subject into message object
+            m.setSubject(subject);
+
+            //set attachment
+            MimeMultipart mimeMultipart = new MimeMultipart();
+
+            //object for set text
+            MimeBodyPart text = new MimeBodyPart();
+
+            //object for set file
+            MimeBodyPart file = new MimeBodyPart();
+
+            //set text message
+            text.setText(message);
+
+            //file path
+            String path = "file path";
+
+            //getting file from path
+            File f = new File(path);
+
+            //setting file
+            file.attachFile(f);
+
+            //setting two body part into multipart attachment object
+            mimeMultipart.addBodyPart(text);
+            mimeMultipart.addBodyPart(file);
+
+            //set multipart file into message object
+            m.setContent(mimeMultipart);
+
+            //sending email
+            Transport.send(m);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
 }
